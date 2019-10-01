f3c8e4f2fd84e237f3bdeb670b96b0bdf5e2d303
Max Bos <maxn.bos@gmail.com>
2019-10-01 16:46:51 +0200

    worked on model

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import argparse
 import torch
 import os
 import numpy as np
+import torchtext
+import torchtext.datasets as datasets
 
 from model import Model
 
@@ -9,14 +11,28 @@ from model import Model
 def main():
   print('Starting process...')
 
-  model = Model()
+  train_iter, test_iter = datasets.IMDB.iters(batch_size=ARGS.batch_size)
+  
+  model = Model(
+    ntokens=200,
+    d_model=ARGS.d_model, nhead=ARGS.nhead, num_layers=ARGS.num_layers,
+    device=DEVICE,
+  )
   model.to(DEVICE)
 
   cross_entropy = torch.nn.CrossEntropyLoss()
   optimizer = torch.optim.Adam(model.parameters())
   
-  for epoch in range(ARGS.epochs):
-    
+  for epoch in range(1, ARGS.epochs + 1):
+    train(train_iter)
+
+
+def train(train_iter):
+  model.train()
+  loss = 0.
+  print(train_iter)
+  for i, batch in enumerate(train_iter):
+    print(batch)
 
 
 if __name__ == "__main__":
@@ -25,11 +41,7 @@ if __name__ == "__main__":
                       help='max number of epochs')
   parser.add_argument('--batch_size', default=4, type=int,
                       help='batch size')
-  parser.add_argument('--device', default='cpu', type=str,
-                      help='device')
-  parser.add_argument('--data_dir', default='data', type=str,
-                      help='directory where the data is stored')
 
   ARGS = parser.parse_args()
-  DEVICE = torch.device(ARGS.device)
+  DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
   main()
--- model.py
+++ model.py
@@ -1,22 +1,53 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+from stochastic_neuron import StochasticNeuron
+
+
+class PositionalEncoding(nn.Module):
+  """
+  Positional encoding.
+  """
+
+  def __init__(self, d_model, dropout=0.1, max_len=5000):
+    super(PositionalEncoding, self).__init__()
+    self.dropout = nn.Dropout(p=dropout)
+
+    pe = torch.zeros(max_len, d_model)
+    position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+    div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+    pe[:, 0::2] = torch.sin(position * div_term)
+    pe[:, 1::2] = torch.cos(position * div_term)
+    pe = pe.unsqueeze(0).transpose(0, 1)
+    self.register_buffer('pe', pe)
+
+  def forward(self, x):
+    x = x + self.pe[:x.size(0), :]
+    return self.dropout(x)
 
 
 class Model(nn.Module):
   """"""
 
-  def __init__(self, d_model, nhead, num_layers, pretrained_embeddings, device):
-    self.embedding = nn.Embedding.from_pretrained(
-      pretrained_embeddings, freeze=False).to(device)
+  def __init__(
+    self, ntokens, d_model, nhead, num_layers, device,
+    n_output=2
+  ):
+    self.embedding = nn.Embedding(ntokens, d_model)
+    self.pos_encoder = PositionalEncoding(d_model)
+    self.ste = StochasticNeuron()
     encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead)
     self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
-    self.fc = nn.Linear(100, 2)
+    self.fc = nn.Linear(d_model, n_output)
 
   def forward(self, input):
     """"""
     embedded = self.embedding(input)
-    out = self.ste(embedded)
+    pos_encoded = self.pos_encoder(embedded)
+    out = self.ste(pos_encoded)
     out = self.transformer_encoder(out)
+    print('out transformer', out.size())
     out = self.fc(out)
     return F.sigmoid(out)
