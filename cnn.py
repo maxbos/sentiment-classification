7fc5773f62c940c1a1d4fb23c8bfd46374fb6f42
joeri1324 <joeri.sleegers@student.uva.nl>
2019-10-05 15:39:49 +0200

    minor changes

--- cnn.py
+++ cnn.py
@@ -4,32 +4,54 @@ import torch.nn.functional as F
 from stochastic_neuron import StochasticNeuron
 
 class CNN(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim, 
-                 dropout, pad_idx):
+    def __init__(
+        self,
+        vocab_size,
+        ste,
+        embedding_dim,
+        filter_amount,
+        kernel_sizes,
+        classes_amount,
+        dropout,
+        pad_idx
+    ):
         super(CNN, self).__init__()
 
-        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx = pad_idx)
-        self.ste = StochasticNeuron()
-        self.convs = nn.ModuleList([
-            nn.Conv2d(in_channels = 1, 
-                      out_channels = n_filters, 
-                      kernel_size = (fs, embedding_dim)) 
-            for fs in filter_sizes
-          ])
-        self.fc = nn.Linear(len(filter_sizes) * n_filters, output_dim)
+        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx=pad_idx)
+        self.ste = StochasticNeuron('REINFORCE') if ste == 'REINFORCE' else StochasticNeuron()
+        self.ste_type = ste
+
+        kernels = []
+        for size in kernel_sizes:
+            k = nn.Conv2d(
+                in_channels=1,
+                out_channels=filter_amount,
+                kernel_size=(size, embedding_dim)
+            ) 
+            kernels.append(k)
+
+        self.kernels = nn.ModuleList(kernels)
+        self.attention = nn.Linear(len(kernel_sizes) * filter_amount, classes_amount)
         self.dropout = nn.Dropout(dropout)
         
     def forward(self, text):
-        #text = [batch size, sent len]
-        embedded = self.ste(self.embedding(text))
-        #embedded = [batch size, sent len, emb dim]
-        embedded = embedded.unsqueeze(1)
-        #embedded = [batch size, 1, sent len, emb dim]
-        conved = [F.relu(conv(embedded)).squeeze(3) for conv in self.convs]
-        #conved_n = [batch size, n_filters, sent len - filter_sizes[n] + 1]
-        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
-        #pooled_n = [batch size, n_filters]
-        cat = self.dropout(torch.cat(pooled, dim = 1))
-        #cat = [batch size, n_filters * len(filter_sizes)]    
-        return self.fc(cat)
+        if self.ste_type in ['REINFORCE', 'STE']:
+            embeddings = self.ste(self.embedding(text))
+        else:
+            embeddings = self.embedding(text)
+
+        embedded = embeddings.unsqueeze(1)
+  
+        convoluted = []
+        for kernel in self.kernels:
+            convoluted.append(F.relu(kernel(embedded)).squeeze(3))
+
+        maximum = []
+        for c in convoluted:
+            maximum.append(F.max_pool1d(c, c.shape[2]).squeeze(2))
+
+        concatenated = torch.cat(maximum, dim=1)
+
+        logits = self.dropout(concatenated)
+        return self.attention(logits)
   
